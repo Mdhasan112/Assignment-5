9fb7c0f8e5be85cd3be88b7181cb28c7f543d2e5
hasan <mdhasanhk.98@gmail.com>
2022-08-09 15:14:10 -0400

    food

---
+++ script.js
@@ -0,0 +1,32 @@
+const searchFood = () => {
+    const searchIteam = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchIteam}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFood(data.meals))
+}
+
+const displayFood = food => {
+
+    const showDetailDiv = document.getElementById('show-details');
+    showDetailDiv.innerHTML = ' ';
+
+    food.forEach(foods => {
+        // console.log(foods.strMeal)
+        const div = document.createElement('div');
+        div.className = 'food-list'
+        div.innerHTML = `
+        <div> ${foods.strMeal} </div>
+        <h3> ${foods.strArea}</h3>
+        <img src="${foods.strMealThumb}">
+        <button onclick="displayDetails()"> Details</button>
+        `;
+
+        showDetailDiv.appendChild(div)
+    })
+    
+}
+
+const displayDetails = details => {
+    console.log(details)
+}
